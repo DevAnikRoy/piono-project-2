a109ee0872d2ee3697a33f49ce78ea531653c8b3
DevAnikRoy <anikroy302@gmail.com>
2025-06-04 01:01:38 +0600

    adding the section

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    {/* main bg section */}
+      <div className='flex items-center justify-center mt-60'>
+        <div className='bg-[#2b2b2b] w-[850px] h-[350px] rounded-xl flex justify-center items-center '>
+          <div className=' bg-white w-1/4 h-[150px] m-5 rounded-xl'>
+            {/* <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>A</h1>
+            <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>W</h1>
+            <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>S</h1>
+            <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>E</h1>
+            <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>D</h1> */}
+          </div>
+          <div className='bg-white w-2/4 h-[150px] m-5 rounded-xl'>
+            <div>F</div>
+            <div>G</div>
+            <div>Y</div>
+            <div>P</div>
+            <div>.</div>
+          </div>
+          <div className='bg-white w-1/4 h-[150px] m-5 rounded-xl'>
+            <div>K</div>
+            <div>O</div>
+            <div>L</div>
+            <div>P</div>
+            <div>.</div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
