24b298f61360534a30f2c6db53c7cca4f1b9a50b
DevAnikRoy <anikroy302@gmail.com>
2025-06-05 01:48:09 +0600

    developed and thoght

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,11 @@ function App() {
       <div className='flex items-center justify-center mt-60'>
         <div className='bg-[#2b2b2b] w-[850px] h-[350px] rounded-xl flex justify-center items-center '>
           <div className=' bg-white w-1/4 h-[150px] m-5 rounded-xl'>
-            {/* <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>A</h1>
+            <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>A</h1>
             <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>W</h1>
             <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>S</h1>
             <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>E</h1>
-            <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>D</h1> */}
+            <h1 className='w-2/5 h-[20rem] bg-white rounded-xl '>D</h1>
           </div>
           <div className='bg-white w-2/4 h-[150px] m-5 rounded-xl'>
             <div>F</div>
